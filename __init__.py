3a406993672570827ce97f35d991bdfc3fccf2f3
Hmz <84510275+HmzSmt@users.noreply.github.com>
2024-11-20 16:20:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -39,25 +39,22 @@ def histogramme():
 
 @app.route("/commits/")
 def commits():
-    
+    # URL de l'API GitHub
     url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
     response = urlopen(url)
     raw_content = response.read()
     commits_data = json.loads(raw_content.decode('utf-8'))
 
-    # Extraire les minutes
     commits_per_minute = {}
     for commit in commits_data:
         date_string = commit['commit']['author']['date']
         date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
         minute = date_object.minute
 
-       
         if minute not in commits_per_minute:
             commits_per_minute[minute] = 0
         commits_per_minute[minute] += 1
 
-    
     results = [{'minute': minute, 'commits': count} for minute, count in commits_per_minute.items()]
     return jsonify(results=results)
 
