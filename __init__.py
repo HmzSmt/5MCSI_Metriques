3de342f1c4446a1ac3c6fd04e7cbc25278abea61
Hmz <84510275+HmzSmt@users.noreply.github.com>
2024-11-20 15:15:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,23 +14,25 @@ def MaPremiereAPI():
 
 @app.route('/tawarano/')
 def meteo():
-  
+   
     url = 'https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx'
 
-    
     response = urlopen(url)
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
 
-    
     results = []
     for list_element in json_content.get('list', []):
         dt_value = list_element.get('dt')  
-        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15  
         results.append({'Jour': dt_value, 'temp': temp_day_value})
 
     
     return jsonify(results=results)
+
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
